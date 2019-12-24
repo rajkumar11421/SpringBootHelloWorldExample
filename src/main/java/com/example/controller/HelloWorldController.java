7351273aa4a819a60042ee54397f15dc33970290
akuma189 <akuma189@LHTU05CG84251TZ.ms.ds.uhc.com>
2019-12-24 16:16:08 +0530

    adding get and post methods

--- src/main/java/com/example/controller/HelloWorldController.java
+++ src/main/java/com/example/controller/HelloWorldController.java
@@ -1,14 +1,21 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-	@RequestMapping
-	public String printHelloWorld(){
-		return "Hello World !!";
+	@RequestMapping(value = "getMessage", method = RequestMethod.GET, produces = { "application/json" })
+	public String getHelloWorld(){
+		return "Get: Hello World !!";
+	}
+	
+	@RequestMapping(value = "postMessage", method = RequestMethod.POST, consumes = {"application/json" }, produces = { "application/json" })
+	public String postHelloWorld(@RequestBody String request){
+		return "Post: Hello World !!";
 	}
 	
 }
