a444901e00355c15830ae8b8d35916c6b05bf863
akuma189 <akuma189@LHTU05CG84251TZ.ms.ds.uhc.com>
2019-12-24 11:33:56 +0530

    First commit

---
+++ src/test/java/com/example/SpringBootHelloWorldExample/SpringBootHelloWorldExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootHelloWorldExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloWorldExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
